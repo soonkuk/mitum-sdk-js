586020c685a63c00ed1ae05378e835ecd6cb6f1f
wyuinche <wyuinche.mido@gmail.com>
2022-11-25 15:26:49 +0900

    fix : export usable classes

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 import { useV, useId, forceExtendedMessage } from "./utils/config.js";
 import { TimeStamp } from "./utils/time.js";
+import { sha256, sum256 } from "./utils/hash.js";
+import { jsonStringify, exportJson } from "./utils/json.js";
+import { Big, Float } from "./utils/number.js";
+import { name, sortStringAsBuf, sortBuf } from "./utils/string.js";
+
+import {
+	SUFFIX_KEY_PRIVATE,
+	SUFFIX_KEY_PUBLIC,
+	SUFFIX_ACCOUNT_ADDRESS,
+} from "./alias/key.js";
+
+import { ID, CurrencyID, ContractID } from "./base/ID.js";
+import { error, assert } from "./base/error.js";
+import { Hint } from "./base/hint.js";
+import { IBytes, IDict, IBytesDict } from "./base/interface.js";
+import { Token } from "./base/token.js";
 
 import { ecdsa } from "./key/ecdsa-keypair.js";
 import { schnorr } from "./key/schnorr-keypair.js";
 import { PublicKey, Keys } from "./key/key.js";
-import { schnorrRandomN, ecdsaRandomN } from "./key/address.js";
+import { Address, schnorrRandomN, ecdsaRandomN } from "./key/address.js";
 
+import { Fact } from "./operations/fact.js";
+import { Item } from "./operations/item.js";
 import { Operation } from "./operations/operation.js";
 
 import { Amount } from "./operations/currency/amount.js";
@@ -80,15 +98,58 @@ const Currency = {
 	WithdrawsFact,
 };
 
+const alias = {
+	key: {
+		SUFFIX_KEY_PRIVATE,
+		SUFFIX_KEY_PUBLIC,
+		SUFFIX_ACCOUNT_ADDRESS,
+	},
+};
+
+const err = {
+	...error,
+	assert,
+};
+
+const base = {
+	IBytes,
+	IDict,
+	IBytesDict,
+};
+
+const util = {
+	sha256,
+	sum256,
+	jsonStringify,
+	exportJson,
+	name,
+	sortStringAsBuf,
+	sortBuf,
+};
+
 export {
 	useV,
 	useId,
 	forceExtendedMessage,
+	alias,
+	err,
+	base,
+	util,
 	TimeStamp,
 	KPGen,
 	PubKey,
 	Keys,
 	Amount,
+	Token,
+	Address,
+	Hint,
+	Fact,
+	Item,
+	ID,
+	Big,
+	Float,
+	CurrencyID,
+	ContractID,
 	Operation,
 	Currency,
 };
